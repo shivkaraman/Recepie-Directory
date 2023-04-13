2580033926c5748d5d56a14245433dc66b6a0253
Shikaraman S <83532128+shivkaraman@users.noreply.github.com>
2023-04-14 00:11:05 +0530

    Color themes added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/home/Home'
 import Recepie from './pages/recepie/Recepie'
 import Create from './pages/create/Create'
 import Search from './pages/search/Search'
+import ThemeSelector from './components/ThemeSelector'
 
 import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom'
 
@@ -15,6 +16,7 @@ function App() {
 		<div className="App">
 			<BrowserRouter>
 				<Navbar/>
+				<ThemeSelector/>
 				<Switch>
 					<Route exact path='/'>
 						<Home/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,12 @@ import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 
 import Searchbar from './Searchbar'
+import { useTheme } from '../hooks/useTheme';
 
 export default function Navbar() {
+    const { color } = useTheme()
     return (
-        <div className='navbar'>
+        <div className='navbar' style={{background: color}}>
             <nav>
                 <NavLink to='/' className='brand'>
                     <h1>The Tasty Table</h1>
