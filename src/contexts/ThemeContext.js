5bd1f32d62944e7b9b0d69c37ba11e90c7b3c19f
Shikaraman S <83532128+shivkaraman@users.noreply.github.com>
2023-04-21 00:08:36 +0530

    Default theme updated

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -16,7 +16,7 @@ const themeReducer = (state, action) => {
 export function ThemeProvider ({ children }) {
     const [state, dispatch] = useReducer(themeReducer, {
         color: '#58249e',
-        mode: 'dark'
+        mode: 'light'
     })
 
     const changeColor = (color) => {
